2ebdbc80eba4a74c9b8fd995daf44eb135613bf6
Alina Kholcheva <akholcheva@gmail.com>
2015-08-08 14:34:11 -0700

    discrete sensitivity function

--- Backbeater/SoundLib/SoundProcessor.m
+++ Backbeater/SoundLib/SoundProcessor.m
@@ -15,9 +15,9 @@
 
 
 
-#define kStartThreshold 0.15
-#define kEndThreshold 0.1
-#define kTimeout 250000000 // 250 000 000ns = 250ms
+//#define kStartThreshold 0.15
+//#define kEndThreshold 0.1
+#define kTimeout 200000000 // 200 000 000ns = 250ms
 
 @interface SoundProcessor()
 
@@ -36,6 +36,8 @@ Float32 _testMaxEnergy;
 Float32 _startThreshold;
 Float32 _endThreshold;
 Float32 _timeout;
+
+NSArray *_startThresholdArray = @[@10, @9, @8, @7, @6, @5, @4.5, @4, @3.5, @3, @2.5, @2.4, @2.3, @2.2, @2.1, @2, @1.95, @1.9, @1.85, @1.8, @1.75, @1.7, @1.65, @1.6, @1.55, @1.54, @1.53, @1.52, @1.51, @1.5, @1.475, @1.45, @1.425, @1.4, @1.39, @1.38, @1.37, @1.36, @1.35, @1.34, @1.33, @1.32, @1.31, @1.3, @1.29, @1.28, @1.27, @1.26, @1.25, @1.24, @1.23, @1.22, @1.21, @1.2, @1.19, @1.18, @1.17, @1.16, @1.15, @1.14, @1.12, @1.1, @1.07, @1.03, @1, @0.95, @0.9, @0.83, @0.75, @0.65, @0.55, @0.45, @0.35, @0.25, @0.15, @0.1, @0.09, @0.08, @0.06, @0.02, @0.015, @0.01, @0.0075, @0.005, @0.003, @0.002, @0.0015, @0.001, @0.0009, @0.0008, @0.0007, @0.0006, @0.0005, @0.00045, @0.0004, @0.00035, @0.0003, @0.00025, @0.0002, @0.00015, @0.0001];
 
 int _strikeCount;
 
@@ -56,8 +58,8 @@ BOOL _strikeState;
     self = [super init];
     if (self != nil) {
         
-        _startThreshold = kStartThreshold;
-        _endThreshold = kEndThreshold;
+//        _startThreshold = kStartThreshold;
+//        _endThreshold = kEndThreshold;
         _timeout = kTimeout;
         
         [self updateStartThreshold];
@@ -274,20 +276,25 @@ float IDLE_TIMEOUT = 5.0;
 
 -(void) updateStartThreshold
 {
-    float sensitivity = (float)Settings.sharedInstance.sensitivity;
-    float A = 0;
-    float B = 0;
-    float C = 0;
+//    float sensitivity = (float)Settings.sharedInstance.sensitivity;
+//    float A = 0;
+//    float B = 0;
+//    float C = 0;
+//    
+//    if (sensitivity < 20){
+//        A = 7; B = 197; C = 19;
+//    } else if (sensitivity < 90) {
+//        A = 1; B = 95; C = 25;
+//    } else {
+//        A = 3; B = 310; C = 200;
+//    }
+//    
+//    _startThreshold = - (A * sensitivity - B ) / C;
     
-    if (sensitivity < 20){
-        A = 7; B = 197; C = 19;
-    } else if (sensitivity < 90) {
-        A = 1; B = 95; C = 25;
-    } else {
-        A = 3; B = 310; C = 200;
-    }
-    
-    _startThreshold = - (A * sensitivity - B ) / C;
+    NSInteger sensitivity = Settings.sharedInstance.sensitivity;
+    _startThreshold = ((NSNumber*)_startThresholdArray[sensitivity]).floatValue;
+    _endThreshold = 1.1 * _startThreshold;
+    NSLog(@"%f - %f", _startThreshold, _endThreshold);
 }
 
 
