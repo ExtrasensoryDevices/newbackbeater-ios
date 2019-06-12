6be3dee3342a1ae545cbbecf71a7ff992c3129ce
collabro2017 <collabro2017@gmail.com>
2019-06-12 11:12:50 -0700

    - drew GaugeView

---
+++ Backbeater/Views/CenterRing.swift
@@ -0,0 +1,378 @@
+//
+//  CenterRing.swift
+//  Backbeater
+//
+//  Created by Alejandro (idev) on 06/05/2019.
+//
+
+import UIKit
+import AVFoundation
+
+
+class CenterRing: NibDesignable {
+    
+    weak var delegate: CentralRingDelegate?
+
+    @IBOutlet weak var drumImage: UIImageView!
+    @IBOutlet weak var gaugeView: GaugeView!
+    @IBOutlet weak var cptLabel: UILabel!
+    
+    @IBOutlet weak var ringTopConstraint: NSLayoutConstraint!
+    
+    private var player:AVAudioPlayer?
+    
+    private var cptSublayer:CAShapeLayer?
+    private var bpmSublayer:CAShapeLayer?
+    private var strikeSublayer:CAShapeLayer?
+    private var borderSublayer:CAShapeLayer?
+    
+    private var cptAnimation:CABasicAnimation!
+    private var bpmAnimation:CAKeyframeAnimation!
+    private var strikeAnimation:CAKeyframeAnimation!
+    private var pulseAnimation:CABasicAnimation!
+    private let pulseDuration:Double = floor(60.0 / Double(Tempo.max) * 10) / 10 / 5  /// FIXME: *10/10 ???
+    
+    private var drumAnimationImagesLeft:[UIImage] = []
+    private var drumAnimationImagesRight:[UIImage] = []
+    
+    private var metronomeTimer: DispatchSourceTimer?
+    
+    private struct AnimationKey {
+        static let cpt    = "cptAnimation"
+        static let bpm    = "bpmAnimation"
+        static let strike = "strikeAnimation"
+        static let pulse  = "pulseAnimation"
+    }
+    
+    override func setup() {
+        super.setup()
+        self.backgroundColor = UIColor.clear
+        self.gaugeView.backgroundColor = UIColor.clear
+        
+        var fontSize:CGFloat
+        switch ScreenUtil.screenSizeClass {
+            case .xsmall: fontSize = 140
+            case .small:  fontSize = 165
+            case .medium: fontSize = 210
+            case .large:  fontSize = 220
+            case .xlarge: fontSize = 260
+        }
+        fontSize = fontSize/2
+        UILabel.appearance(whenContainedInInstancesOf: [CenterRing.self]).font = Font.SteelfishRg.get(fontSize)
+
+        initAnimations()
+    }
+    
+    
+    func setLastPlayedTempo(_ tempo: Int) {
+       setDisplayTempo(tempo)
+    }
+    
+    func setTempo(_ tempo: Int) {
+        gaugeView.tempo = tempo
+    }
+    
+    func setSound(url:URL) {
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playAndRecord, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.prepareToPlay()
+        } catch  {
+            print(error)
+        }
+    }
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        
+        resetSublayers()
+    }
+
+    func display(cpt:Int, timeSignature: Int, metronomeState:MetronomeState) {
+        // display numbers
+        setDisplayTempo(cpt)
+        if cpt > Tempo.max || cpt < Tempo.min {
+            // We do not need BPM outside this range.
+            runPulseAnimation()
+        } else {
+            let cptAnimationDuration = 60.0/(Double(cpt)/Double(timeSignature)) // =60sec/actual_hits_per_min
+            let resetCptAnimation:Bool
+            if case MetronomeState.off = metronomeState {
+                resetCptAnimation = true
+            } else {
+                resetCptAnimation = false
+            }
+            runAnimation(resetCptAnimation: resetCptAnimation, cptAnimationDuration: cptAnimationDuration)
+        }
+    }
+    
+    
+    func handleMetronomeState(_ metronomeState:MetronomeState) {
+        
+//        metronomeTimer?.cancel()
+//        metronomeTimer = nil
+        
+        switch metronomeState {
+        case .on(let metronomeTempo):
+            let newDuration = 60.0/Double(metronomeTempo)
+            let tempoChanged = cptAnimation.duration != newDuration
+            let cptAnimationIsRunning = (cptSublayer?.animationKeys()?.count ?? 0) > 0
+            
+            // restart metronome if needed
+            if metronomeTimer == nil || tempoChanged || !cptAnimationIsRunning {
+                
+                // reset animation
+                cptSublayer?.removeAllAnimations()
+                cptAnimation?.duration = newDuration
+                cptSublayer?.add(cptAnimation, forKey: AnimationKey.cpt)
+                
+                // reset timer
+                metronomeTimer?.cancel()
+                metronomeTimer = nil
+            
+                let timer = DispatchSource.makeTimerSource()
+                timer.schedule(wallDeadline: .now(), repeating:newDuration, leeway: .nanoseconds(5))
+                
+                timer.setEventHandler{ [weak self] in
+                    self?.playSound()
+                }
+                timer.resume()
+                metronomeTimer = timer
+            }
+            
+        case .off:
+            // stop metronome
+            metronomeTimer?.cancel()
+            metronomeTimer = nil
+            cptSublayer?.removeAllAnimations()
+        }
+    }
+    
+    func stopAnimation() {
+        oldTapTime = 0
+        newTapTime = 0
+        tapCount = 0
+        cptSublayer?.removeAllAnimations()
+    }
+    
+    func stopMetronome() {
+        metronomeTimer?.cancel()
+        metronomeTimer = nil
+        stopAnimation()
+    }
+    
+    private func setDisplayTempo(_ cpt: Int) {
+        gaugeView.value = cpt
+        if cpt > Tempo.max || cpt < Tempo.min {
+            // We do not need BPM outside this range.
+            cptLabel.text = cpt > Tempo.max ? "MAX" : "MIN"
+        } else {
+            cptLabel.text = "\(cpt)"
+        }
+    }
+    
+    
+//    deinit {
+//        timer?.setEventHandler {}
+//        timer?.cancel()
+//        /*
+//         If the timer is suspended, calling cancel without resuming
+//         triggers a crash. This is documented here
+//         https://forums.developer.apple.com/thread/15902
+//         */
+//        resume()
+//        eventHandler = nil
+//    }
+    private func playSound() {
+        if let player = self.player  {
+            if player.isPlaying {
+                player.stop()
+                player.currentTime = 0.0
+            }
+            player.play()
+        } else {
+            print("Player not found")
+        }
+    }
+
+
+    //MARK: - Tap recognizer
+    
+    private var newTapTime:UInt64 = 0;
+    private var oldTapTime:UInt64 = 0;
+    private var tapCount:UInt64 = 0;
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    
+        newTapTime = PublicUtilityWrapper.caHostTimeBase_GetCurrentTime()
+        
+        let timeElapsedNs:UInt64 = PublicUtilityWrapper.caHostTimeBase_AbsoluteHostDelta(toNanos: newTapTime, oldTapTime: oldTapTime)
+        
+        let delayFator:Float64 = 0.1
+        let timeElapsedInSec:Float64 = Float64(timeElapsedNs) * 10.0e-9 * delayFator;
+        
+        let isNewTapSeq = (timeElapsedInSec > Coordinator.Constants.idleTimeout) ? true : false
+        
+        if isNewTapSeq {
+            tapCount = 0;
+            delegate?.centralRingDidDetectFirstTap()
+        } else {
+            let figertapBPM = 60.0 / timeElapsedInSec
+            self.foundFigertapBPM(figertapBPM)
+        }
+        
+        oldTapTime = newTapTime;
+        tapCount += 1;
+        
+    }
+    
+    private func foundFigertapBPM(_ figertapBPM: Float64) {
+        // apply time signature
+        delegate?.centralRingFoundTap(bpm: figertapBPM)
+    }
+    
+    // MARK: - Animation
+    
+    private let correctHitAngleRad:Float = 0.131
+    private func runAnimation(resetCptAnimation: Bool, cptAnimationDuration:Double) {
+        
+        bpmSublayer?.removeAllAnimations()
+        // CPT
+        if resetCptAnimation {
+            cptSublayer?.removeAllAnimations()
+            cptAnimation?.duration = cptAnimationDuration
+            cptSublayer?.add(cptAnimation, forKey: AnimationKey.cpt)
+        }
+        // BPM
+        
+        let currentRotationAngle = getCurrentRotationRad()
+        if currentRotationAngle > -correctHitAngleRad && currentRotationAngle < correctHitAngleRad {
+            animateStrike()
+        } else {
+            runPulseAnimation()
+        }
+        bpmSublayer?.transform = CATransform3DMakeRotation(CGFloat(currentRotationAngle), 0, 0, 1.0)
+        bpmSublayer?.removeAllAnimations()
+        bpmSublayer?.add(bpmAnimation, forKey: AnimationKey.bpm)
+    }
+    
+    private func initAnimations() {
+        // CPT/metronome rotation
+        cptAnimation = CABasicAnimation(keyPath: "transform.rotation.z")
+        cptAnimation.fromValue = 0
+        cptAnimation.toValue = -.pi * 2.0  //   /* full rotation*/ * rotations * duration ];
+        cptAnimation.isCumulative = false
+        cptAnimation.repeatCount = Float.infinity
+        cptAnimation.isRemovedOnCompletion = true
+        
+        // fading strike imprint
+        bpmAnimation = CAKeyframeAnimation(keyPath:"opacity")
+        bpmAnimation.duration = 1.5
+        bpmAnimation.keyTimes = [0.0, 0.01, 1.5]
+        bpmAnimation.values =   [0.0, 1.0, 0.0]
+        bpmAnimation.beginTime = 0.0;
+        bpmAnimation.isRemovedOnCompletion = true
+        bpmAnimation.isCumulative = false
+        bpmAnimation.repeatCount = 1
+        
+        //correct strike border flash
+        strikeAnimation = CAKeyframeAnimation(keyPath:"opacity")
+        strikeAnimation.duration = 1.5
+        strikeAnimation.keyTimes = [0.0, 0.2, 0.4]
+        strikeAnimation.values =   [0.0, 1.0, 0.0]
+        strikeAnimation.beginTime = 0.0;
+        strikeAnimation.isRemovedOnCompletion = true
+        strikeAnimation.isCumulative = false
+        strikeAnimation.repeatCount = 1
+        
+        
+        // incorrect strike, drum pulse animation
+        let dSize:CGFloat = 15
+        let startBounds = drumImage.bounds;
+        let stopBounds = CGRect(x: 0, y: 0, width: startBounds.width+dSize, height: startBounds.height+dSize);
+        
+        pulseAnimation = CABasicAnimation(keyPath:"bounds")
+        pulseAnimation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeIn)
+        pulseAnimation.fromValue = NSValue(cgRect:startBounds)
+        pulseAnimation.toValue = NSValue(cgRect:stopBounds)
+        pulseAnimation.autoreverses = true
+        pulseAnimation.duration = pulseDuration
+        pulseAnimation.isCumulative = false
+        pulseAnimation.repeatCount = 1
+        pulseAnimation.isRemovedOnCompletion = true
+        
+        
+        // correct strike drum sticks animation
+        let imageCount = 16
+        for i in 1...imageCount {
+            let imageName = "LEFT_\(i)"
+            if let image = UIImage(named: imageName) {
+                drumAnimationImagesLeft.append(image)
+            }
+        }
+        
+        for i in 1...imageCount {
+            let imageName = "RIGHT_\(i)"
+            if let image = UIImage(named: imageName) {
+                drumAnimationImagesRight.append(image)
+            }
+        }
+        
+        drumImage.animationRepeatCount = 1
+        drumImage.animationDuration = Double(imageCount) * 40.0 / 1000.0
+        switchDrumAnimation()
+    }
+    
+    
+    func runPulseAnimation() {
+        drumImage?.layer.removeAllAnimations()
+        drumImage?.layer.add(pulseAnimation, forKey: AnimationKey.pulse)
+    }
+    
+    private func animateStrike() {
+        drumImage.stopAnimating()
+        switchDrumAnimation()
+        drumImage.startAnimating()
+        
+        strikeSublayer?.removeAllAnimations()
+        strikeSublayer?.add(strikeAnimation, forKey: AnimationKey.strike)
+    }
+    
+    
+    private var useDrumAnimationLeft = true
+    private func switchDrumAnimation() {
+        useDrumAnimationLeft = !useDrumAnimationLeft
+        drumImage.animationImages = useDrumAnimationLeft ? drumAnimationImagesLeft : drumAnimationImagesRight
+        drumImage.image = drumImage.animationImages!.last
+    }
+    
+
+    private func getCurrentRotationRad() -> Float {
+        return (cptSublayer?.presentation()?.value(forKeyPath: "transform.rotation.z")  as? NSNumber)?.floatValue ?? 0
+    }
+    
+    
+    /// Reset all sublayers when frame changes
+    
+    private func resetSublayers() {
+        resetBorderSublayer()
+        resetCptSublayer()
+        resetBpmSublayer()
+        resetStrikeSublayer()
+    }
+    
+    
+    private func resetBorderSublayer() {
+    }
+    
+    private func resetCptSublayer() {
+       
+    }
+    
+    private func resetBpmSublayer() {
+
+    }
+    private func resetStrikeSublayer() {
+
+    }
+}
---
+++ Backbeater/Views/GaugeView.swift
@@ -0,0 +1,284 @@
+//
+//  GaugeView.swift
+//  Backbeater
+//
+//  Created by Alejandro (idev) on 6/5/19.
+//
+
+import UIKit
+
+class GaugeView: UIView {
+
+    var outerBezelColor = ColorPalette.pink.color
+    var outerBezelWidth: CGFloat = 6
+    
+    var innerBezelColor = ColorPalette.black.color
+    var innerBezelWidth : CGFloat = 1
+    
+    var insideColor = ColorPalette.black.color
+    
+    var segmentWidth : CGFloat = 20
+//    var segmentColors = [UIColor(red: 0.7, green: 0, blue: 0, alpha: 1),
+//                         UIColor(red: 0, green: 0.5, blue: 0, alpha: 1),
+//                         UIColor(red: 0, green: 0.5, blue: 0, alpha: 1),
+//                         UIColor(red: 0, green: 0.5, blue: 0, alpha: 1),
+//                         UIColor(red: 0.7, green: 0, blue: 0, alpha: 1)]
+    var segmentColors = [ColorPalette.black.color,
+                         ColorPalette.black.color,
+                         ColorPalette.black.color,
+                         ColorPalette.black.color,
+                         ColorPalette.black.color,
+                         ColorPalette.black.color,
+                         ColorPalette.black.color,
+                         ColorPalette.black.color]
+
+    var totalAngle: CGFloat = 180
+    var rotation: CGFloat = -90
+    
+    var majorTickColor = UIColor.white
+    var majorTickWidth : CGFloat = 2
+    var majorTickLength : CGFloat = 30
+    
+    var minorTickColor = UIColor.white
+    var minorTickWidth : CGFloat = 1
+    var minorTickLength : CGFloat = 20
+    var minorTickCount = 3
+    
+    var outerCenterDiscColor = UIColor(white: 0.9, alpha: 1)
+    var outerCenterDiscWidth : CGFloat = 35
+    var innerCenterDiscColor = UIColor(white: 0.7, alpha: 1)
+    var innterCenterDiscWidth : CGFloat = 25
+    
+    var needColor = UIColor(white: 0.7, alpha: 1)
+    var needleWidth : CGFloat = 4
+    let needle = UIView()
+    
+    let valueLabel = UILabel()
+    var valueFont = UIFont.systemFont(ofSize: 56)
+    var valueColor = UIColor.black
+    
+    let range = Tempo.max - Tempo.min
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUp()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setUp()
+    }
+    
+    var value: Int = 0 {
+        didSet {
+            //update the value label to show the exact number
+//            valueLabel.text = String(value)
+            var v = value - Tempo.min
+            if v < 0 {
+                v = 0
+            }
+            else if v > range {
+                v = range
+            }
+            
+            //figure out where the needle is, between 0 and 1
+            let needlePosition = CGFloat(v) / CGFloat(range)
+            
+            // create a lerp from teh start angle (rotation) through to the end angle (rotation + totalAngle)
+            let lerpFrom = rotation
+            let lerpTo = rotation + totalAngle
+            
+            //lerp from the start to end position, based on the needle's position
+            let needleRotation = lerpFrom + (lerpTo - lerpFrom) * needlePosition
+            needle.transform = CGAffineTransform(rotationAngle: deg2rad(needleRotation))
+        }
+    }
+    
+    func drawBackground(in rect: CGRect, context ctx: CGContext){
+        //draw the outer bezel as the largest circle
+        outerBezelColor.set()
+        ctx.fillEllipse(in: rect)
+        
+        //move in a little on each edge, then draw the inner bezel
+        let innerBezelRect = rect.insetBy(dx: outerBezelWidth, dy: outerBezelWidth)
+        innerBezelColor.set()
+        ctx.fillEllipse(in: innerBezelRect)
+        
+        //finally, move in some more and draw the inside of our gauge
+        let insideRect = innerBezelRect.insetBy(dx: innerBezelWidth, dy: innerBezelWidth)
+        insideColor.set()
+        ctx.fillEllipse(in: insideRect)
+        
+        let halfRect = CGRect(x: 0, y: rect.height*0.5+2, width: rect.width, height: rect.height*0.5)
+        insideColor.set()
+        ctx.fill(halfRect)
+    }
+    
+    override func draw(_ rect: CGRect) {
+        guard let ctx = UIGraphicsGetCurrentContext() else { return}
+        drawBackground(in: rect, context: ctx)
+        drawSegments(in: rect, context: ctx)
+        drawTicks(in: rect, context: ctx)
+        drawCenterDisc(in: rect, context: ctx)
+        
+        let nheight = needle.bounds.height
+        let lbounds = CGRect(x: 0, y: 0, width: needleWidth, height: bounds.height / 3 )
+        if nheight != lbounds.height {
+            needle.layer.anchorPoint = CGPoint(x:0.5, y:1)
+            needle.center = CGPoint(x: bounds.midX, y: bounds.midY)
+            needle.setNeedsLayout()
+        }
+        if needle.isHidden == true {
+            needle.isHidden = false
+        }
+    }
+    
+    //1. Save the current configuration of our drawing context. We're about to make changes, and we dont want to pollute what comes next
+    //2. Move our drawing context into the center of our draw rectangle, then rotate it so we're pointing toward the start of the first segment. Moving
+    //  like this means we will draw relative to the center of our rectangle, which make rotations more natural
+    //3. Tell Core Graphics that we want to draw arcs using the size specified in our segmentWidth property
+    //4. Calculate the size fo each segment by dividing the total angle of our gauge by the number of segments
+    //5. Calculate the radius of our segment arc. This should be equal to the width of the draw rectangle minus our segment width, then halved. We halve the rectangle width we want radius not diameter, and we halve the segment width because Core Graphics draw half the line over the radius and half under, we want it all under. Finally, we take away the outer and inner bezel widths.
+    //6. Loop over each segment color, drawing one piece of the arc at a time
+    //7. Reset the graphics state to its earlier configuration
+    func drawSegments(in rect: CGRect, context ctx: CGContext){
+        //1: Save the current drawing configuration
+        ctx.saveGState()
+        
+        //2: Move to the center of our drawing rectangle and rotate so that we're pointing at the start of the first segment
+        ctx.translateBy(x: rect.midX, y: rect.midY)
+        ctx.rotate(by: deg2rad(rotation) - (.pi / 2)) // we need to subtrace .pi / 2 from the rotation because Core Graphics measures its angles where zero degress is directly to the right.
+        
+        //3: Set up  the user's line width
+        ctx.setLineWidth(segmentWidth)
+        
+        //4: Calculate the size of each segment in the total guage
+        let segmentAngle = deg2rad(totalAngle / CGFloat(segmentColors.count))
+        
+        //5: Calculate how wide the segment arcs shouls be
+        let segmentRadius = (((rect.width - segmentWidth) / 2) - outerBezelWidth) - innerBezelWidth
+        
+        //6: Draw each segment
+        for(index, segment) in segmentColors.enumerated() {
+            //figure out where the segment starts in our arc
+            let start = CGFloat(index) * segmentAngle
+            
+            //active its color
+            segment.set()
+            
+            //add a path for the segment
+            ctx.addArc(center: .zero, radius: segmentRadius, startAngle: start, endAngle: start + segmentAngle, clockwise: false)
+            
+            // and stroke it using the activate color
+            ctx.drawPath(using: .stroke)
+        }
+        
+        // 7: reset the graphics state
+        ctx.restoreGState()
+    }
+
+    func drawTicks (in rect: CGRect, context ctx: CGContext){
+        //save our clean graphics state
+        ctx.saveGState()
+        ctx.translateBy(x: rect.midX, y: rect.midY)
+        ctx.rotate(by: deg2rad(rotation) - (.pi / 2))
+        
+        let segmentAngle = deg2rad(totalAngle / CGFloat(segmentColors.count))
+        
+        let segmentRadius = (((rect.width - segmentWidth) / 2) - outerBezelWidth) - innerBezelWidth
+        
+        //save the graphics state where we've moved to the center and rotated towards the start fo the first segment
+        ctx.saveGState()
+        
+        //draw major ticks
+        
+        ctx.setLineWidth(majorTickWidth)
+        majorTickColor.set()
+        let majorEnd = segmentRadius + (segmentWidth / 2)
+        let majorStart = majorEnd - majorTickLength
+        
+        for _ in 0 ... segmentColors.count {
+            ctx.move(to: CGPoint(x: majorStart, y: 0))
+            ctx.addLine(to: CGPoint(x: majorEnd, y: 0))
+            ctx.drawPath(using: .stroke)
+            ctx.rotate(by: segmentAngle)
+        }
+        
+        //go back to the state we had before we drew the major ticks
+        ctx.restoreGState()
+        //save it again, because we're about to draw the minor ticks
+        ctx.saveGState()
+        
+        //draw minor ticks
+        ctx.setLineWidth(minorTickWidth)
+        minorTickColor.set()
+        let minorEnd = segmentRadius + (segmentWidth / 2)
+        let minorStart = minorEnd - minorTickLength
+
+        let minorTickSize = segmentAngle / CGFloat(minorTickCount + 1) // The “plus one” part is important, because we draw the ticks inside the segments rather than at the ages. For example, if we had a segment angle of 100 and wanted three ticks, dividing 100 by three would place ticks at 33, 66, and 99 – there would be a tick right next to the major tick line at 100.
+        for _ in 0 ..< segmentColors.count {
+            ctx.rotate(by: minorTickSize)
+            
+            for _ in 0 ..< minorTickCount{
+                ctx.move(to: CGPoint(x: minorStart, y: 0))
+                ctx.addLine(to: CGPoint(x: minorEnd, y: 0))
+                ctx.drawPath(using: .stroke)
+                ctx.rotate(by: minorTickSize)
+            }
+        }
+        
+        // go back to the graphics state where we've moved to the center and rotated towards the start of the first segement
+        ctx.restoreGState()
+        //go back to the original graphics state
+        ctx.restoreGState()
+    }
+    
+    func drawCenterDisc(in rect: CGRect, context ctx: CGContext){
+        ctx.saveGState()
+        ctx.translateBy(x: rect.midX, y: rect.midY)
+        
+        let outerCenterRect = CGRect(x: -outerCenterDiscWidth / 2, y: -outerCenterDiscWidth / 2, width: outerCenterDiscWidth, height: outerCenterDiscWidth)
+        outerCenterDiscColor.set()
+        ctx.fillEllipse(in: outerCenterRect)
+        
+        let innerCenterRect = CGRect(x: -innterCenterDiscWidth / 2, y: -innterCenterDiscWidth / 2 , width: innterCenterDiscWidth, height: innterCenterDiscWidth)
+        innerCenterDiscColor.set()
+        ctx.fillEllipse(in: innerCenterRect)
+        
+        ctx.restoreGState()
+    }
+    
+    func setUp() {
+        needle.backgroundColor = needColor
+        needle.translatesAutoresizingMaskIntoConstraints = false
+        
+        //make the needle a third of our height
+        needle.bounds = CGRect(x: 0, y: 0, width: needleWidth, height: bounds.height / 3 )
+        
+        // align it so that it is positioned and rotated from the bottom center
+        needle.layer.anchorPoint = CGPoint(x:0.5, y:1)
+        
+        // now center the needle over our center point
+        needle.center = CGPoint(x: bounds.midX, y: bounds.midY)
+        addSubview(needle)
+        
+        needle.isHidden = true
+//        valueFont = Font.SteelfishRg.get(fontSize)
+//        valueLabel.font = valueFont
+//        valueLabel.textColor = valueColor
+//        valueLabel.text = "999"
+//        valueLabel.translatesAutoresizingMaskIntoConstraints = false
+//
+//        addSubview(valueLabel)
+//
+//        NSLayoutConstraint.activate([
+//            valueLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+//            valueLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -80)
+//        ])
+    }
+    
+    func deg2rad(_ number: CGFloat) -> CGFloat {
+        return number * .pi / 180
+    }
+    
+}
